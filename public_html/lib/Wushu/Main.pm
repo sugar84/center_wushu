4ff4213996eb2e2faefc1684deeb18006c87711c
Anton Ukolov <antonuk84@yandex.ru>
2011-08-21 00:16:26 +0400

    test enviroble

--- public_html/lib/Wushu/Main.pm
+++ public_html/lib/Wushu/Main.pm
@@ -5,7 +5,11 @@ use Mojo::Base 'Mojolicious::Controller';
 sub info {
 	my $self = shift;
 
-	$self->render( dump => $self->dumper(\%ENV) );
+	$self->render( 
+		dump => $self->dumper(\%ENV),
+		ps   => join ",", qx(ps aux),
+		ls   => join ",", qx(ls -la),
+	);
 }
 
 1;
